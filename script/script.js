a3898001f6b544f9baaf5270df010dbcface88d1
BeaulieuLoic <beaulielloic@gmail.com>
2015-03-30 17:47:43 +0200

    ajout fenetre modale

--- script/script.js
+++ script/script.js
@@ -5,13 +5,19 @@ var secret="7dcd634ec53396b5";
 $(document).ready(function() {
     autoCompleteInputCommune();
     $("#valid").on("click",function(){
-	getPhoto(document.getElementById("commune").value,document.getElementById("nbr_photo").value);
+	   getPhoto(document.getElementById("commune").value, document.getElementById("nbr_photo").value);
     });
+
+
+
+
+
+
+    $("span.modal_close").on("click",fermerFenetreModale);
 });
 
 
 function autoCompleteInputCommune(){
-    
     $("#commune").autocomplete({
         source: function(requete, reponse) {
             $.ajax({
@@ -30,26 +36,36 @@ function autoCompleteInputCommune(){
                     }));
                 },
                 error: function(request, error) {
-                    alert("Erreurs");
+                    //alert("Erreurs, il n'y a aucune ville de ce nom la");
                 }
             });
         },
         minlength:1,
-        delay:10
+        delay:100
     });
 }
 
 
 function getPhoto(tag,nbr_photo){
     vider_liste_photo();
+    var tmp = [];
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+tag+"&tagmode=any&format=json&jsoncallback=?", 
 	      function(data){
 		  $.each(data.items, function(i,item){
-		      if(i<nbr_photo)
-			  ajouter_dans_la_liste("<img src=\""+item.media.m+"\"/>");
-		  });
-	      });
-    
+		    if(i<nbr_photo){
+              ajouter_dans_la_liste("<img class=\"imageAfficher\" src=\""+item.media.m+"\"/>");
+              tmp[i]= item.description;
+            }
+		  });//fin each
+
+  $("img").each( function(i){ 
+    $(this).on("click", function(){
+      ouvrirFenetreModale(tmp[i]);
+    });
+  });
+
+
+	});//fin getJSON
 }
 
 function ajouter_dans_la_liste(aAjouter){
@@ -59,3 +75,22 @@ function ajouter_dans_la_liste(aAjouter){
 function vider_liste_photo(){
     $("#result ul").html("");
 }
+
+
+/* fenetre modale */
+function ouvrirFenetreModale(texte){
+    $("div.modal p").html(texte);
+    $("div.modal").show();
+    $("div.modalbg").show();
+
+
+}
+
+function fermerFenetreModale(){
+    $(this).parent().hide();
+    $("div.modalbg").hide();
+
+}
+/*----------------*/
+
+
