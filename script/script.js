55051a2b328aa10c08ceeeb475573aeb9a9b2b89
Prygan <prygan@gmail.com>
2015-04-04 17:34:06 +0200

    ebauche css

--- script/script.js
+++ script/script.js
@@ -6,25 +6,25 @@ $(document).ready(function() {
 
     autoCompleteInputCommune();
     $("#valid").on("click",function(){
-      getPhoto($("#commune").val(), $("#nbr_photo").val());
+	getPhoto($("#commune").val(), $("#nbr_photo").val());
     });
 
     $("#tri #auteur").on("click", function(){
-      actualiseAffichageImg(tableTrierPar(listeImage,"auteur"));
+	actualiseAffichageImg(tableTrierPar(listeImage,"auteur"));
     });
 
     $("#tri #date").on("click", function(){
-      actualiseAffichageImg(tableTrierPar(listeImage,"date"));
+	actualiseAffichageImg(tableTrierPar(listeImage,"date"));
     });
 
     $("span.modal_close").on("click",fermerFenetreModale);
     
     $("#disposition #caroussel").on("click", function(){
-      actualiseDisposition();
+	actualiseDisposition();
     });
 
     $("#disposition #liste").on("click", function(){
-      actualiseDisposition();
+	actualiseDisposition();
     });
 
 
@@ -48,9 +48,6 @@ function autoCompleteInputCommune(){
                             }
                         }
                     }));
-                },
-                error: function(request, error) {
-                  alert("Erreurs, il n'y a aucune ville de ce nom la");
                 }
             });
         },
@@ -66,20 +63,20 @@ function getPhoto(tag,nbr_photo){
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+tag+"&tagmode=any&format=json&jsoncallback=?", 
 	      function(data){
 		  $.each(data.items, function(i,item){
-		    if(i<nbr_photo){
-          var tmp = "imageAfficher"+i;
-          if ($("#filtreDate").val()!=""){
-            date = parseDate($("#filtreDate").val())
-            if (item.date_taken >= date) {
-              listeImage[i]={"id": tmp, "url":item.media.m, "description":item.description, "titre": item.title, "date":item.date_taken, "auteur": item.author};
-            };
-          }else{  
-            listeImage[i]={"id": tmp, "url":item.media.m, "description":item.description, "titre": item.title, "date":item.date_taken, "auteur": item.author};  
-          }
-        }
+		      if(i<nbr_photo){
+			  var tmp = "imageAfficher"+i;
+			  if ($("#filtreDate").val()!=""){
+			      date = parseDate($("#filtreDate").val())
+			      if (item.date_taken >= date) {
+				  listeImage[i]={"id": tmp, "url":item.media.m, "description":item.description, "titre": item.title, "date":item.date_taken, "auteur": item.author};
+			      };
+			  }else{  
+			      listeImage[i]={"id": tmp, "url":item.media.m, "description":item.description, "titre": item.title, "date":item.date_taken, "auteur": item.author};  
+			  }
+		      }
 		  });//fin each items
-      actualiseAffichageImg(listeImage);
-	});//fin getJSON
+		  actualiseAffichageImg(listeImage);
+	      });//fin getJSON
 }
 
 function ajouter_dans_la_liste(aAjouter){
@@ -108,123 +105,123 @@ function fermerFenetreModale(){
 
 function parseDate(date)
 {
-  var mja = date.split("/");
-  return mja[2] + "-" + mja[0] + "-" + mja[1] + "T00:00:00-00:00";
+    var mja = date.split("/");
+    return mja[2] + "-" + mja[0] + "-" + mja[1] + "T00:00:00-00:00";
 }
 
 
 
 function sortString(field){
     return function(a,b){
-      var x = a[field].toLowerCase();
-      var y = b[field].toLowerCase();
-      if (x < y){
-        return -1;
-      }
-      if (x > y){
-        return 1;
-      }
-      return 0;
+	var x = a[field].toLowerCase();
+	var y = b[field].toLowerCase();
+	if (x < y){
+            return -1;
+	}
+	if (x > y){
+            return 1;
+	}
+	return 0;
     }
 }
 
 function tableTrierPar(table, trierPar){
-  return table.sort(sortString(trierPar));
+    return table.sort(sortString(trierPar));
 }
 
 function actualiseAffichageImg(table){
-  vider_liste_photo();
+    vider_liste_photo();
 
-  for (var i = 0; i < table.length; i++) {
-    img = table[i];
-    id = "#"+img["id"];
+    for (var i = 0; i < table.length; i++) {
+	img = table[i];
+	id = "#"+img["id"];
 
-    ajouter_dans_la_liste("<img id=\""+img["id"]+"\" src=\""+img["url"]+"\""+
-                          " onclick=\"imgOnClick("+i+")\""+
-                          "/>");
-  };
+	ajouter_dans_la_liste("<img id=\""+img["id"]+"\" src=\""+img["url"]+"\""+
+                              " onclick=\"imgOnClick("+i+")\""+
+                              "/>");
+    };
 
-  actualiseDisposition();
+    actualiseDisposition();
 }
 function imgOnClick(positionImage){
     ouvrirFenetreModale("titre:"+listeImage[positionImage]["titre"]+
-      "</br>"+
-      listeImage[positionImage]["description"]+
-      "</br> date: "+
-      listeImage[positionImage]["date"]+
-      "</br> auteur: "+
-      listeImage[positionImage]["auteur"]);
+			"</br>"+
+			listeImage[positionImage]["description"]+
+			"</br> date: "+
+			listeImage[positionImage]["date"]+
+			"</br> auteur: "+
+			listeImage[positionImage]["auteur"]);
 }
 
 
 // Jcarousel //
 function removeCaroussel(){
-  $("#aSupr").html("");
-  $(".jcarousel li").css("float","none");
-  $(".jcarousel-wrapper").css("border","");
+    $("#aSupr").html("");
+    $(".jcarousel li").css("float","none");
+    $(".jcarousel-wrapper").css("border","");
 
 }
 
 function caroussel(){
 
-  $(".jcarousel-wrapper").css("border","2px solid black");
-  var tmp = "<a href=\"#\" class=\"jcarousel-control-prev\">&lsaquo;</a>"+
+    $(".jcarousel-wrapper").css("border","2px solid black");
+    var tmp = "<a href=\"#\" class=\"jcarousel-control-prev\">&lsaquo;</a>"+
         "<a href=\"#\" class=\"jcarousel-control-next\">&rsaquo;</a>"+
-                  
-        "<p class=\"jcarousel-pagination\">";
+        
+    "<p class=\"jcarousel-pagination\">";
 
-  $("#aSupr").html("");
-  $("#aSupr").append(tmp);
-  
-  $(".jcarousel li").css("float", "left");
+    $("#aSupr").html("");
+    $("#aSupr").append(tmp);
+    
+    $(".jcarousel li").css("float", "left");
 
 
-  (function($) {
-      $(function() {
-          $('.jcarousel').jcarousel();
+    (function($) {
+	$(function() {
+            $('.jcarousel').jcarousel();
 
-          $('.jcarousel-control-prev')
-              .on('jcarouselcontrol:active', function() {
-                  $(this).removeClass('inactive');
-              })
-              .on('jcarouselcontrol:inactive', function() {
-                  $(this).addClass('inactive');
-              })
-              .jcarouselControl({
-                  target: '-=1'
-              });
+            $('.jcarousel-control-prev')
+		.on('jcarouselcontrol:active', function() {
+                    $(this).removeClass('inactive');
+		})
+		.on('jcarouselcontrol:inactive', function() {
+                    $(this).addClass('inactive');
+		})
+		.jcarouselControl({
+                    target: '-=1'
+		});
 
-          $('.jcarousel-control-next')
-              .on('jcarouselcontrol:active', function() {
-                  $(this).removeClass('inactive');
-              })
-              .on('jcarouselcontrol:inactive', function() {
-                  $(this).addClass('inactive');
-              })
-              .jcarouselControl({
-                  target: '+=1'
-              });
+            $('.jcarousel-control-next')
+		.on('jcarouselcontrol:active', function() {
+                    $(this).removeClass('inactive');
+		})
+		.on('jcarouselcontrol:inactive', function() {
+                    $(this).addClass('inactive');
+		})
+		.jcarouselControl({
+                    target: '+=1'
+		});
 
-          $('.jcarousel-pagination')
-              .on('jcarouselpagination:active', 'a', function() {
-                  $(this).addClass('active');
-              })
-              .on('jcarouselpagination:inactive', 'a', function() {
-                  $(this).removeClass('active');
-              })
-              .jcarouselPagination();
-      });
-  })(jQuery);
+            $('.jcarousel-pagination')
+		.on('jcarouselpagination:active', 'a', function() {
+                    $(this).addClass('active');
+		})
+		.on('jcarouselpagination:inactive', 'a', function() {
+                    $(this).removeClass('active');
+		})
+		.jcarouselPagination();
+	});
+    })(jQuery);
 }
 
 function actualiseDisposition(){
-  var select = document.getElementById("disposition");
-  var elemSelected = select.options[select.selectedIndex].value;
+    var select = document.getElementById("disposition");
+    var elemSelected = select.options[select.selectedIndex].value;
 
-  if (elemSelected == "en caroussel") {
-    caroussel();
-  }else{
-    removeCaroussel();
-  }
+    if (elemSelected == "en caroussel") {
+	caroussel();
+    }else{
+	removeCaroussel();
+    }
 }
 // Jcarousel //
